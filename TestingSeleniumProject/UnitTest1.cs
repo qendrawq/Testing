ae560ba518dc19313a00e669ced347d656a32511
Andrew <39080198+andrew@users.noreply.github.com>
2019-02-27 12:09:05 +0200

    Добавьте файлы проекта.

---
+++ TestingSeleniumProject/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestingSeleniumProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
